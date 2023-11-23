212000015d3e71544070d9c6e5a423a870c74e68
Dagson Gabriel <dagson.g.01@gmail.com>
2023-11-23 14:42:39 -0300

    Nat - Multiplicação

--- Nat.hs
+++ Nat.hs
@@ -5,6 +5,11 @@ import Prelude hiding (sum)
 data Nat = O | S Nat
     deriving ( Eq , Show )
 
+
 sum :: Nat -> Nat -> Nat
 sum n O = n
 sum n (S m) = S (sum n m)
+
+mult :: Nat -> Nat -> Nat
+mult n O = O
+mult n (S m) = sum n (mult n m)
