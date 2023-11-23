a05ea05804c22322050756d3c03fa0e596aa8355
Dagson Gabriel <dagson.g.01@gmail.com>
2023-11-23 16:03:00 -0300

    Bool - 'not' para implementar Odd a partir do Even

--- Bool.hs
+++ Bool.hs
@@ -2,7 +2,7 @@
 {-# HLINT ignore "Use camelCase" #-}
 module Bool where
 
-import Prelude hiding (True, False, Bool)
+import Prelude hiding (True, False, Bool, not)
 import Nat
 
 data Bool = False | True
@@ -13,6 +13,10 @@ if_then_else_ :: Bool -> a -> a -> a
 if_then_else_ True n _ = n
 if_then_else_ False _ m = m
 
+not :: Bool -> Bool
+not True = False
+not False = True
+
 leq :: Nat -> Nat -> Bool
 leq (S n) (S m) = leq n m
 leq O _ = True
@@ -22,3 +26,6 @@ ev :: Nat -> Bool
 ev (S (S n)) = ev n
 ev O = True
 ev _ = False
+
+odd :: Nat -> Bool
+odd n = not (ev n)
